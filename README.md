0b7af4eaff1615e42c4b23eb249d7e6c5abd9766
formaseja <formaseja@gmail.com>
2023-07-18 19:38:26 -0600

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Autenticación
-Ejemplo de autenticación con Firebase y Google.
+# bdfalumnos.github.io
+Ejemplo De Conexión a la B.D con Firebase
--- js/init.js
+++ js/init.js
@@ -1,5 +1,11 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyDsUpL8uR1Zr0et6koLnPOtFTSUNHd8wbI",
@@ -9,4 +15,4 @@ firebase.initializeApp({
   messagingSenderId: "1010558580925",
   appId: "1:1010558580925:web:b3db0c22cadca5f8ac9fd4",
   measurementId: "G-M1RWBPEEQT"
-    });
+});
