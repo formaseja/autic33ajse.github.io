feab7692d64dbe174b44396615f3553ed6dda742
formaseja <formaseja@gmail.com>
2023-07-18 19:06:55 -0600

    cambios

--- js/init.js
+++ js/init.js
@@ -8,11 +8,5 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDsUpL8uR1Zr0et6koLnPOtFTSUNHd8wbI",
-  authDomain: "autic33ase.firebaseapp.com",
-  projectId: "autic33ase",
-  storageBucket: "autic33ase.appspot.com",
-  messagingSenderId: "1010558580925",
-  appId: "1:1010558580925:web:b3db0c22cadca5f8ac9fd4",
-  measurementId: "G-M1RWBPEEQT"
+
 });
